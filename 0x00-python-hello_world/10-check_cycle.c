105ce25f60a2191b919d8f106b028503aaeedacd
BeboNewEra <abanoubsamy2341@gmail.com>
2023-10-02 22:32:28 +0300

    betty added

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,15 +1,22 @@
 #include "lists.h"
 
+/**
+ * check_cycle - function to check cyclic linked lists
+ * @list: list
+ * Return: 1 or 0 on sucess
+ *
+ *
+ */
+
 int check_cycle(listint_t *list)
 {
 	if (list == NULL || list->next == NULL)
-	{
 		return 0;
-	}
+
 	listint_t *buffer = list;
 	listint_t *fastbuffer = list;
 
-	while (fastbuffer != NULL || fastbuffer->next != NULL)
+	while (fastbuffer && fastbuffer->next)
 	{
 		buffer = buffer->next;
 		fastbuffer = fastbuffer->next->next;

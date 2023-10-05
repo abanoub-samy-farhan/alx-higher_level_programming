a5f4501ae33dde9f99c6a26bf27872c607366b23
BeboNewEra <abanoubsamy2341@gmail.com>
2023-10-05 06:38:59 +0300

    Python import tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    import sys
+
+    lenght = len(sys.argv) - 1
+    if lenght == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(lenght))
+
+    for i in range(lenght):
+        print("{} : {}".format(i + 1, sys.argv[i + 1]))

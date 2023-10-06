eeea47b88e03134bce61b38068816ffba2fb0bbd
BeboNewEra <abanoubsamy2341@gmail.com>
2023-10-06 17:39:15 +0300

    100 advanced

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    def add(x, y):
+        return x + y
+
+    def sub(x, y):
+        return x - y
+
+    def mul(x, y):
+        return x * y
+
+    def div(x, y):
+        return x / y
+
+    operator = {
+            '*' : mul,
+            '+' : add,
+            '-': sub,
+            '/': div
+            }
+    length = len(sys.argv)
+    if length - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        if sys.argv[2] not in operator:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        else:
+            func = operator[sys.argv[2]]
+            result = func(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} + {} = {}".format(int(sys.argv[1]), int(sys.argv[3]), result))
+    exit(0)

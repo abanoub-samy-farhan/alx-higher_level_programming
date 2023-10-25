3834c3cb8ca877ee8dcb564462c784c720e5320d
BeboNewEra <abanoubsamy2341@gmail.com>
2023-10-25 17:32:56 +0300

    error handling

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as te:
-        error_msg = "Exception:" + str(te) + "\n"
+    except Exception as te:
+        error_msg = "Exception: " + str(te) + "\n"
         sys.stderr.write(error_msg)
         return False

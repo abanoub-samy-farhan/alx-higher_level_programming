883c145ed91d39d93da425e0e0cb6ecc0ec2e4b3
BeboNewEra <abanoubsamy2341@gmail.com>
2023-10-25 17:02:18 +0300

    printing integer of divison

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     value = 0
+
     try:
         value = a / b
-    except (ZeroDivisionError):
+    except ZeroDivisionError:
         value = None
     finally:
-        print("inside result: {}".format(value))
+        print("Inside result: {}".format(value))
         return value

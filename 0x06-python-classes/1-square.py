85d3f34ec5546936bbe307732e1e0fb89e88b847
BeboNewEra <abanoubsamy2341@gmail.com>
2023-10-26 13:31:57 +0300

    task 1 done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Here goes the first class"""
+
+
+class Square:
+    """Defines the class: Square"""
+
+
+    def __init__(self, size):
+        """Constructors
+        Args:
+            size: lenght of the square.
+        """
+        self.__size = size
+    

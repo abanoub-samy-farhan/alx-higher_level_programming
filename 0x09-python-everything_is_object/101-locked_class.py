316a0d09246cf1b0cdeda56ef53fa0e09ba32934
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-01 20:24:27 +0200

    locked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,13 +6,4 @@ class LockedClass:
     """
     Prevents any variable to be added in the class
     """
-    def __init__(self):
-        """Define the main and only instance"""
-        self.first_name = None
-
-    def __setattr__(self, name, value):
-        """set attribute according to the allowed ones"""
-        if name == "first_name":
-            super().__setattr__(name, value)
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+    __slots__ = ("first_name")

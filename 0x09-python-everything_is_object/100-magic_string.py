c2349690f9c66f3016da2b506f504758f7e39f60
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-01 18:47:03 +0200

    getattr used

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
-class static_variable:
-    count = 0
-
 def magic_string():
-    static_variable.count += 1
-    string = "BestSchool, " * static_variable.count
-    return string[:-2]
+    magic_string.count = getattr(magic_string, "count", 0) + 1
+    return ", ".join("BestSchool" for i in range(magic_string.count))

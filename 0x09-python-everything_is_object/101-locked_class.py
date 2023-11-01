d34e61e765c6915a3a2eaa7f5ce298abfd45863e
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-01 19:11:27 +0200

    find the locked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,4 +6,13 @@ class LockedClass:
     """
     Prevents any variable to be added in the class
     """
-    __slots__ = ("first_name")
+    def __init__(self):
+        """Define the main and only instance"""
+        self.first_name = None
+
+    def __setattr__(self, name, value):
+        """set attribute according to the allowed ones"""
+        if name == "first_name":
+            super().__setattr__(name, value)
+        else:
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))

51978eb11b2614d61d547072b67cb0bacff2c27c
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-08 04:38:53 +0200

    file 6 fix

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,6 +2,9 @@
 """defining the function in the place"""
 
 
+import json
+
+
 def load_from_json_file(filename):
     """returning the file json"""
     with open(filename, "r", encoding="utf-8") as f:

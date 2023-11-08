1e63bb075d6dda87e926f9dc4c9f00ce15677693
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-08 03:22:51 +0200

    file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """returns the text in the file"""
-    with open(filname, "w", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)

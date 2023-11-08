f7db365652e24d13d7d5f56e12c8cac396d10f8f
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-08 03:18:04 +0200

    file edited

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """reads filename with utf8"""
-    with open(filename, encoding = 'utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         print(f.read(), end='')

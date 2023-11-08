c0c7c237bd0aa85c2752704221c3cc0504c3c303
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-08 04:53:58 +0200

    git add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,10 +9,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 args = list(sys.argv[1:])
 
 try:
-    old_date = load_from_json_file('add_item.json')
+    old_data = load_from_json_file('add_item.json')
 except Exception:
-    old_date = []
+    old_data = []
 
 
-old_date.extend(args)
-save_to_json_file(old_date, "add_item.json")
+old_data.extend(args)
+save_to_json_file(old_data, "add_item.json")

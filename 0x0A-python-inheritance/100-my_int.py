229f643a17a2fa687d5cba09819518ffd67c5470
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-10 20:00:35 +0200

    file 100 done

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""python is a good language"""
+
+class MyInt(int):
+    """int but has some cool stuff"""
+
+    def __eq__(self, value):
+        """returning the revarse of the normal function"""
+        return super().__ne__(value)
+
+    def __ne__(self, value):
+        """returning the reverse value of it"""
+        return super().__eq__(value)

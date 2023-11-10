49bff21c45040d977b2cd030d6731c86eb66947e
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-10 17:38:04 +0200

    file 3 done

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""file to check the inheritancy rate"""
+
+
+def is_same_class(obj, a_class):
+    """returing true on suc, false on fail"""
+    return isinstance(obj, a_class)

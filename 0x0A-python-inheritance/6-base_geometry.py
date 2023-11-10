64c03dc7f88ad3a01185216f1145e8b3ee9270a1
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-10 18:20:06 +0200

    file 6 done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" file git into the bases of the geometry """
+
+
+class BaseGeometry:
+    """ a class that do sth i don't know yet"""
+
+    def area(self):
+        """ not yet done to receive anything """
+        raise Exception("area() is not implemented")

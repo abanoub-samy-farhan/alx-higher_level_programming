e6cedace74f8b304efb88bf3075510ee97d38311
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-10 19:05:13 +0200

    tests done

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -21,3 +21,7 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(self.name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(self.name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/7-base_geometry.txt")

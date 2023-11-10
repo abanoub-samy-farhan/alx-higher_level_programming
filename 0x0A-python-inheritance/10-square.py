3cbc86e2e390179ef1889dc4d5ee3ca5fcc75761
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-10 19:45:08 +0200

    test 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""returns another inherited lib"""
+Rectangle = __import__("8-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """another inherited lib for another work"""
+    def __init__(self, size):
+        """defining the constructors of the project"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """returning the area of the square"""
+        return self.__size ** 2

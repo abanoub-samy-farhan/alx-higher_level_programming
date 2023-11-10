9dd94dc739431be8f86f10be7da3e603cbfefed8
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-10 17:45:57 +0200

    inheritance check

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""python is getting to be easy as a, b, c again"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ returning the True on sucsess, and False on fail"""
+    return isinstance(obj, a_class)

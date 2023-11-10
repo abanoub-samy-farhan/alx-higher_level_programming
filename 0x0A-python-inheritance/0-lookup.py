67e859a50f38714553a164f18b738cf0ca186eb2
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-10 17:12:29 +0200

    file done

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""python is getting harder and harder"""
+
+
+def lookup(obj):
+    """returing a dictionary of the object"""
+    return obj.__dict
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+Readme file for the inheritance project, very interesting

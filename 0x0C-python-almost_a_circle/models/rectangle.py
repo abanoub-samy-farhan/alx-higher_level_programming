659d39a348ed941cc752dad9aab8c143e7ae696d
BeboNewEra <abanoubsamy2341@gmail.com>
2023-11-14 03:42:24 +0200

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,7 +99,7 @@ class Rectangle(Base):
         elif kwargs:
             self.__update(**kwargs)
 
-     def to_dictionary(self):
+    def to_dictionary(self):
         '''Returns dictionary'''
         return {"id": self.id, "width": self.__width, "height": self.__height,
                 "x": self.__x, "y": self.__y}

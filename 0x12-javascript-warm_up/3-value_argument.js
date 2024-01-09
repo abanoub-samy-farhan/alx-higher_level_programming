fcb7ad3fa741a7a5a1ee8600a71e44213e471847
BeboNewEra <abanoubsamy2341@gmail.com>
2024-01-09 02:34:24 +0200

    last file 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,5 @@
 if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  process.argv.slice(2).forEach((arg, index) => {
-    console.log(arg);
-  });
+  console.log(process.argv[2]);
 }

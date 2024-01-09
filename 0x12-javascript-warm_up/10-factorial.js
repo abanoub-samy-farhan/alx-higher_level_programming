c485a762ee3ce0089282da9c84241acab4e65534
BeboNewEra <abanoubsamy2341@gmail.com>
2024-01-09 03:55:12 +0200

    file 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,11 +4,15 @@ const argument = process.argv[2];
 const argInt = parseInt(argument);
 let flat = 1;
 
-if (isNaN(argInt)) {
-  console.log(flat);
-} else if (argInt > 0) {
+function factorial (num) {
   for (let i = 1; i < argInt; i++) {
     flat = flat * i;
   }
+  return flat;
+}
+
+if (isNaN(argInt)) {
   console.log(flat);
+} else if (argInt > 0) {
+  console.log(factorial(argInt));
 }

8311a3ee6d9bbb7c5b81a4c4ded9ecbce3c3c02f
abanoub_farhan <abanoubsamy2341@gmail.com>
2024-06-22 19:54:33 +0300

    edited

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 """
-making the databases more handy and useful in comming days
+Models used in the task
 """
 from sys import argv
 import MySQLdb
 
+
 def main():
-    """this function will be responisble for listing all the states in the database"""
+    """
+    the main function for the porcess
+    """
     db_user = argv[1]
     db_password = argv[2]
-    db_name = argv[3]
+    db_db = argv[3]
     db_host = "localhost"
     db_port = 3306
-    
-    try:
-        db = MySQLdb.connect(host = db_host,user=db_user, passwrd=db_password, db= db_name, port=db_port)
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY name ASC;")
-        querey_result = cur.fetchall()
-        for row in querey_result:
-            print(row)
-        cur.close()
-        db.close()
 
+    try:
+        conn = MySQLdb.connect(host=db_host, port=db_port,
+                               user=db_user, passwd=db_password,
+                               db=db_db, charset="utf8")
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+        query_rows = cursor.fetchall()
+        for row in query_rows:
+            print(row)
+        cursor.close()
+        conn.close()
     except Exception:
         pass
+
 
 if __name__ == "__main__":
     main()

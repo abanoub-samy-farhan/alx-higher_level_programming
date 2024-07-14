e1d21cd4fe960f76c62c7649959437fa86d0bd1a
abanoubfarhan <abanoubsamy2341@gmail.com>
2024-07-15 00:31:11 +0300

    file 2 done

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,8 +5,7 @@ Module documented
 import urllib
 from sys import argv
 
-if __name__ == "__main__":
-    url = argv[1]
-    with urllib.request.urlopen(url) as response:
-        html = response.getheader("X-Request-Id")
-        print(f"{html}")
+url = argv[1]
+with urllib.request.urlopen(url) as response:
+    html = response.getheader("X-Request-Id")
+    print(f"{html}")

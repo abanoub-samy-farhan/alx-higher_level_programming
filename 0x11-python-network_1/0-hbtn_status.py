4deed975411a4a391f5d1ded41bf64562ecdefc6
abanoubfarhan <abanoubsamy2341@gmail.com>
2024-07-15 00:02:31 +0300

    file 1 done

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# making the file urllib first time to use
+from urllib import request
+
+with request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
+    data = resp.read()
+    html = data.decode("UTF-8")
+    print("Body Response:")
+    print(f"\t- type: {type(data)}")
+    print(f"\t- content: {data}")
+    print(f"\t- utf8 content: {html}")
